8868f6e0c4c868e0072ee7bd05ac5ba355583cd8
Jason Burgess <jason.nzd@gmail.com>
2023-03-20 19:00:37 +1300

    debugged nvm install

--- userdata.sh
+++ userdata.sh
@@ -6,7 +6,10 @@ sudo yum install iptables -y
 echo "Installing Node Version Manager NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
+
+. ~/.nvm/nvm.sh
 nvm install 16
+npm install -g npm@9.6.2
 node -e "console.log('Testing Node.js ' + process.version)"
 
 echo "Mapping IP Tables - Routing port 3000 to 80"
@@ -14,6 +17,8 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
 
 git clone https://github.com/Jason-nzd/supermarket-prices-nextjs
 cd supermarket-prices-nextjs
+
+# todo: setup .env secrets
 
 npm install
 npm run build
